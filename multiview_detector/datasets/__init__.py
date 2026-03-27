670b6af113db53dd2c5aec5383074abfdc1fdd7c
chen <chen.jixuanstu@gmail.com>
2026-03-27 17:21:37 +0800

    keep only README

--- multiview_detector/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Wildtrack import Wildtrack
-from .MultiviewX import MultiviewX
-from .Canghai import Canghai
-from .Citystreet import Citystreet
-from .frameDataset import frameDataset
--- multiview_detector/transformer_models/ops/functions/__init__.py
+++
@@ -1,2 +0,0 @@
-from .ms_deform_attn_func import MSDeformAttnFunction, ms_deform_attn_core_pytorch, ms_deform_attn_core_pytorch_mot
-
